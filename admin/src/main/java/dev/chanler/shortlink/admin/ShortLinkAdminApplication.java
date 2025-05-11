c47e9520e7fce99185dd3115ff428cf574b8928d
Chanler <merryfling@gmail.com>
2025-05-11 21:12:30 +0800

    feature: 初始化短链接后管

---
+++ admin/src/main/java/dev/chanler/shortlink/admin/ShortLinkAdminApplication.java
@@ -0,0 +1,16 @@
+package dev.chanler.shortlink.admin;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author: Chanler
+ */
+@SpringBootApplication
+@MapperScan("dev.chanler.shortlink.admin.dao.mapper")
+public class ShortLinkAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShortLinkAdminApplication.class, args);
+    }
+}
