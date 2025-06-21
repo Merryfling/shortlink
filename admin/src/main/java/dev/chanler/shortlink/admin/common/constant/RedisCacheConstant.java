76477585821f105dbeb64dc1190e8a5a37a7cf1f
Chanler <merryfling@gmail.com>
2025-06-22 00:09:09 +0800

    feature: 开发短链接后管系统: 用户注册功能，添加布隆过滤器判段用户名是否占用

---
+++ admin/src/main/java/dev/chanler/shortlink/admin/common/constant/RedisCacheConstant.java
@@ -0,0 +1,9 @@
+package dev.chanler.shortlink.admin.common.constant;
+
+/**
+ * 后管系统 Redis 缓存常量类
+ * @author: Chanler
+ */
+public class RedisCacheConstant {
+    public static final String LOCK_USER_REGISTER_KEY = "short-link:lock:user-register:";
+}
