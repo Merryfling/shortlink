7e958fef13618bc36cf2903b95fce6edd5d3ef4a
Merryfling <merryfling@gmail.com>
2025-07-18 11:39:23 +0800

    feature: 开发短链接后管系统: 用户登录有效期修改为 30 天

--- admin/src/main/java/dev/chanler/shortlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/dev/chanler/shortlink/admin/service/impl/UserServiceImpl.java
@@ -113,7 +113,7 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
          */
         String uuid = UUID.randomUUID().toString();
         stringRedisTemplate.opsForHash().put("login_" + userLoginReqDTO.getUsername(), uuid, JSON.toJSONString(userDO));
-        stringRedisTemplate.expire("login_" + userLoginReqDTO.getUsername(), 30L, TimeUnit.MINUTES);
+        stringRedisTemplate.expire("login_" + userLoginReqDTO.getUsername(), 30L, TimeUnit.DAYS);
         return new UserLoginRespDTO(uuid);
     }
 
