0b200e6a0e45f99a9604355db6899d333fc66e03
Merryfling <merryfling@gmail.com>
2025-07-20 06:46:24 +0800

    feature: 开发短链接系统: 分页插件

---
+++ admin/src/main/java/dev/chanler/shortlink/admin/config/DataBaseConfiguration.java
@@ -0,0 +1,24 @@
+package dev.chanler.shortlink.admin.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author: Chanler
+ */
+@Configuration
+public class DataBaseConfiguration {
+
+    /**
+     * 分页插件
+     */
+    @Bean
+    public MybatisPlusInterceptor myBatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
