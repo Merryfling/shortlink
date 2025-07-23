4c623272018d019b74773b82ab274ebee3d177cc
Merryfling <merryfling@gmail.com>
2025-07-23 08:53:37 +0800

    feature: 开发短链接中台监控系统: 添加短链接分组实体及持久层

---
+++ project/src/main/java/dev/chanler/shortlink/project/dao/entity/GroupDO.java
@@ -0,0 +1,45 @@
+package dev.chanler.shortlink.project.dao.entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import dev.chanler.shortlink.project.common.database.BaseDO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 分组实体
+ * @author: Chanler
+ */
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@TableName("t_group")
+public class GroupDO extends BaseDO {
+
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 分组标识
+     */
+    private String gid;
+
+    /**
+     * 分组名称
+     */
+    private String name;
+
+    /**
+     * 创建分组用户名
+     */
+    private String username;
+
+    /**
+     * 分组排序
+     */
+    private Integer sortOrder;
+}
---
+++ project/src/main/java/dev/chanler/shortlink/project/dao/mapper/GroupMapper.java
@@ -0,0 +1,11 @@
+package dev.chanler.shortlink.project.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import dev.chanler.shortlink.project.dao.entity.GroupDO;
+
+/**
+ * 短链接分组持久层
+ * @author: Chanler
+ */
+public interface GroupMapper extends BaseMapper<GroupDO> {
+}
