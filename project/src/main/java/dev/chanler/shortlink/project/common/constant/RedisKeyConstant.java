efe336af932b54297a592e107352f024507a8c45
Merryfling <merryfling@gmail.com>
2025-07-25 09:21:33 +0800

    feature: 开发短链接中台系统: 更新RedisKeyConstant: 修改格式并添加分组ID锁前缀

--- project/src/main/java/dev/chanler/shortlink/project/common/constant/RedisKeyConstant.java
+++ project/src/main/java/dev/chanler/shortlink/project/common/constant/RedisKeyConstant.java
@@ -8,19 +8,25 @@ public class RedisKeyConstant {
 
     /**
      * 短链接跳转前缀 key
-     * 格式：short-link_goto_{gid}
+     * 格式：short-link:goto:{fullShortUrl}
      */
-    public static final String GOTO_SHORT_LINK_KEY = "short-link_goto_%s";
+    public static final String GOTO_SHORT_LINK_KEY = "short-link:goto:%s";
 
     /**
      * 短链接空值跳转锁前缀 key
-     * 格式：short-link_goto_{gid}
+     * 格式：short-link:is-null:goto_{fullShortUrl}
      */
-    public static final String GOTO_IS_NULL_SHORT_LINK_KEY = "short-link_is-null_goto_%s";
+    public static final String GOTO_IS_NULL_SHORT_LINK_KEY = "short-link:is-null:goto_%s";
 
     /**
      * 短链接跳转锁前缀 key
-     * 格式：short-link_goto_{gid}
+     * 格式：short-link:lock:goto:{fullShortUrl}
      */
-    public static final String LOCK_GOTO_SHORT_LINK_KEY = "short-link_lock_goto_%s";
+    public static final String LOCK_GOTO_SHORT_LINK_KEY = "short-link:lock:goto:%s";
+
+    /**
+     * 短链接修改分组 ID 锁前缀 Key
+     * 格式：short-link:lock:update-gid:{fullShortUrl}
+     */
+    public static final String LOCK_GID_UPDATE_KEY = "short-link:lock:update-gid:%s";
 }
